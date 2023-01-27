bcc00476100e7a61d198a55ea5bd214c43b346a4
Thatcher Auten <thatcherauten24@gmail.com>
2023-01-27 12:33:24 -0600

    fixing bugs

--- FindWord.java
+++ FindWord.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class FindWord {
     public static void main(String[] args) {
         
+        System.out.println("Please enter a sentence or phrase");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        String text2 = text + " ";
+
+        System.out.println("Please enter a letter to search for within your sentence or phrase");
+        String letter = scanner.nextLine();
+
+        int pos = text2.indexOf(letter);
+        String textBeforeWord = text2.substring(0, pos);
+        int wordStartPos = textBeforeWord.lastIndexOf(" ");
+        int wordEndPos = text2.indexOf(" ", pos);
+        
+        if (pos == -1) {
+            System.out.println(letter + " is not in the sentence");
+        } else {
+            System.out.println(text2.substring(wordStartPos + 1, wordEndPos));
+        }
+
+        scanner.close();
     }
 }
